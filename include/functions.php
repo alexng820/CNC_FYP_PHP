bd7008eec7fb4e4497d1c41545e70c654050123c
Ash Chan <ashchan1122@gmail.com>
2017-04-30 01:30:30 +0800

    Remove unused functions

--- include/functions.php
+++ include/functions.php
@@ -1,35 +1,6 @@
 <?php
 require_once 'variables.php';
 
-//Encrypt password using bcrypt
-function bcrypt($password) {
-	$options = array('cost' => 12);
-	return password_hash($password, PASSWORD_BCRYPT, $options);
-}
-
-//Generate UUIDv4
-function gen_uuid() {
-	return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-	// 32 bits for "time_low"
-	mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-
-	// 16 bits for "time_mid"
-	mt_rand( 0, 0xffff ),
-
-	// 16 bits for "time_hi_and_version",
-	// four most significant bits holds version number 4
-	mt_rand( 0, 0x0fff ) | 0x4000,
-
-	// 16 bits, 8 bits for "clk_seq_hi_res",
-	// 8 bits for "clk_seq_low",
-	// two most significant bits holds zero and one for variant DCE1.1
-	mt_rand( 0, 0x3fff ) | 0x8000,
-
-	// 48 bits for "node"
-	mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-	);
-}
-
 //Functions for database operations
 function connectDB() {
 	return mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
